117e73dd5fb4853629f1e6106feb2fc78f56b12c
crayyy_zee <65814383+i-am-zaidali@users.noreply.github.com>
2024-06-13 02:33:14 +0500

    Buncha fixes and additions
    Removed internet connection checking
    Added check for whether the .deb file already exists
    Added check for if the deb file is the latest version to prevent reinstallation
    Now using `dpkg --compare-version` instead of manual comparison for a more streamlined check.
    Used the proper success code (0 for success) if discord doesn't exist, if the current version is the latest version or if discord updated successfully

--- update-discord.sh
+++ update-discord.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 if ! dpkg -l discord >/dev/null 2>&1; then
-  exit 0
+    notify-send "No discord found" "It seems discord isn't installed"
+    exit 0
 fi
-
-function has_internet() {
-  nslookup google.com &> /dev/null
-  return $?
-}
-
-while ! has_internet; do
-  sleep 10
-done
 
 DOWNLOAD_URL="https://discord.com/api/download/stable?platform=linux&format=deb"
 
@@ -20,28 +12,37 @@ FILE_NAME="discord.deb"
 DOWNLOAD_DIR="/tmp"
 
 cur_version=$(dpkg --status discord | awk '/Version:/ {print $2}' | cut -d' ' -f1)
+latest_ver="$(echo "$(curl -Ls -I -o /dev/null -w '%{url_effective}' https://discord.com/api/download/stable?platform=linux\&format=deb)" | cut -d'-' -f2 | cut -d'.' -f1-3)"
 
-latest_ver="$(echo "$(curl -Ls -I -o /dev/null -w %{url_effective} https://discord.com/api/download/stable?platform=linux&format=deb)" | cut -d'-' -f2 | cut -d'.' -f1-3)"
-
-if [[ "$cur_version" == "$latest_ver" ]]; then
-  notify-send "Update Aborted" "Discord is already on the latest version: ${latest_ver}"
-  exit 1
+if ! dpkg --compare-versions "$latest_ver" gt "$cur_version"; then
+    notify-send "Update Aborted" "Discord is already on the latest version: ${latest_ver}" -a discord
+    exit 0
 fi
 
-
-if ! wget -O "$DOWNLOAD_DIR/$FILE_NAME" "$DOWNLOAD_URL"; then
-  notify-send "Error" "Failed to download Discord. Aborting installation."
-  exit 1
+if dpkg-deb --info "$DOWNLOAD_DIR/$FILE_NAME"; then
+    DOWNLOADED_VER=$(dpkg --info "$DOWNLOAD_DIR/$FILE_NAME" | awk '/Version:/ {print $2}' | cut -d' ' -f1)
+else
+    DOWNLOADED_VER="0.0.0"
 fi
 
+if [ ! -f "$DOWNLOAD_DIR/$FILE_NAME" ] || dpkg --compare-versions "$latest_ver" gt "$DOWNLOADED_VER"; then
+    notify-send "Downloading" "Couldn't find an existing download of discord's latest deb package. Downloading from website now."
+    if ! wget -v -O "$DOWNLOAD_DIR/$FILE_NAME" "$DOWNLOAD_URL"; then
+        notify-send "Error" "Failed to download Discord. Aborting installation."
+        exit 1
+    fi
+fi
+
+notify-send "File already exists" "Discord's debian package of version ${latest_ver} already exists. Installing that"
 
 if ! sudo dpkg -i "$DOWNLOAD_DIR/$FILE_NAME"; then
-  notify-send "Error" "Error installing Discord. Trying to install missing dependencies..."
-  sudo apt update && sudo apt install -f
-  if ! sudo dpkg -i "$DOWNLOAD_DIR/$FILE_NAME"; then
-    notify-send "Error" "Failed to install Discord even after dependency installation."
-    exit 1
-  fi
+    notify-send "Error" "Error installing Discord. Trying to install missing dependencies..."
+    sudo apt update && sudo apt install -f
+    if ! sudo dpkg -i "$DOWNLOAD_DIR/$FILE_NAME"; then
+        notify-send "Error" "Failed to install Discord even after dependency installation."
+        exit 1
+    fi
 fi
 
-notify-send "Updated" "Discord has been updated successfully from ${cur_version} to ${latest_ver}')"
+notify-send "Updated" "Discord has been updated successfully from ${cur_version} to ${latest_ver}" -a discord
+exit 0
